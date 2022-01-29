0c57f1330ff2426a4fb2ee2f88fea9ff75fc33d2
RafidMuhymin <rafidmuhymin@gmail.com>
2022-01-29 23:44:03 +0600

    added getImagePath util to the codecs-plugin

--- vite-plugin-astro-codecs/utils.mjs
+++ vite-plugin-astro-codecs/utils.mjs
@@ -57,3 +57,12 @@ export const getConfigOptions = (config, ext, image) => {
       typeof inline === "string",
   };
 };
+
+export const getImagePath = (base, extension, width, hash) => {
+  const name = `${base}@${width}w.${extension}`;
+
+  const assetName = `${base}@${width}w.${hash}.${extension}`;
+
+  const path = `/@assets/${assetName}`;
+  return { name, assetName, path };
+};
