ebeb591c08d2731d39d3d49b9a714bc84fc60b77
RafidMuhymin <rafidmuhymin@gmail.com>
2022-02-12 23:04:21 +0600

    updated astro-imagetools and asedited config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,5 @@ import astroImagePlugin from "astro-imagetools/plugin";
 export default {
   vite: {
     plugins: [astroImagePlugin],
-    optimizeDeps: {
-      exclude: ["@astropub/codecs", "imagetools-core", "sharp"],
-    },
-    ssr: {
-      external: ["sharp", "potrace"],
-    },
   },
 };
